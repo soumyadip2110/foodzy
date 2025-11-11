ae25f41b7cef8d5e2e2183878581ea6e7d9e2f9a
Soumyadip Chowdhury <soumyadip2110@gmail.com>
2025-11-12 00:36:21 +0530

    improve cart section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,6 +16,13 @@ function Home() {
     const [menu, setMenu] = useState(food_items)
     const [isSearching, setIsSearching] = useState(false)
 
+    let subTotal = cartItems.reduce((total, item) => total + item.price, 0);
+    let deliveryCharge = 20;
+    let taxes = parseFloat((subTotal * 0.05).toFixed(2));
+    let total = Math.floor(subTotal + deliveryCharge + taxes);
+    // console.log(subTotal);
+
+
     const filterMenu = (category) => {
         if (category === 'All') setMenu(food_items);
         else {
@@ -73,7 +80,7 @@ function Home() {
                 ))}
             </div>
 
-            <div className={`w-full md:w-[40vw] h-[100%] fixed top-0 right-0 bg-white shadow-xl p-6 ${show ? 'translate-x-0' : 'translate-x-full'} transition-all duration-500`}>
+            <div className={`w-full md:w-[40vw] h-[100%] fixed top-0 right-0 bg-white shadow-xl p-6 ${show ? 'translate-x-0' : 'translate-x-full'} transition-all duration-500 overflow-auto flex flex-col items-center`}>
                 <header className='w-[100%] flex justify-between items-center'>
                     <span className='text-green-400 font-semibold text-[18px]'>Order items</span>
                     <RxCross2
@@ -93,6 +100,28 @@ function Home() {
                         />
                     ))}
                 </div>
+
+                <div className='w-full border-t-2 border-b-2 border-gray-400 mt-7 flex flex-col gap-2 p-5'>
+                    <div className='w-full flex justify-between items-center'>
+                        <span className='text-md text-gray-600 font-semibold'>Sub total</span>
+                        <span className='text-md text-green-400 font-semibold'>Rs {subTotal}/-</span>
+                    </div>
+                    <div className='w-full flex justify-between items-center'>
+                        <span className='text-md text-gray-600 font-semibold'>Delivery charge</span>
+                        <span className='text-md text-green-400 font-semibold'>Rs {deliveryCharge}/-</span>
+                    </div>
+                    <div className='w-full flex justify-between items-center'>
+                        <span className='text-md text-gray-600 font-semibold'>Taxes</span>
+                        <span className='text-md text-green-400 font-semibold'>Rs {taxes}/-</span>
+                    </div>
+                </div>
+                <div className='w-full flex justify-between items-center p-3'>
+                    <span className='text-lg text-gray-600 font-bold'>Total</span>
+                    <span className='text-lg text-green-400 font-bold'>Rs {total}/-</span>
+                </div>
+                <button className='w-[80%] p-3 rounded-lg bg-green-500 text-white hover:bg-green-400 transition-all cursor-pointer'>
+                    Place Order
+                </button>
             </div>
         </div>
     )
